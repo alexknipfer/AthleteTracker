7484f5150880b28d03318db031b5a5676bf21783
joserameriz17 <jja800@lionmail.lindenwood.edu>
2016-02-25 14:54:43 +0000

    modified schools page

--- client/components/Schools/Schools.jsx
+++ client/components/Schools/Schools.jsx
@@ -2,7 +2,40 @@ Schools = React.createClass({
   render(){
     return (
      <div>
-        <h1>Schools</h1>
+        <ul class="collection">
+          <li class="collection-item avatar">
+            <span class="title"><strong> Fen Creek High School</strong></span>
+            <p>Phone Number: 314-999-9000 <br></br>
+               Coach Name:   Steven Lewis <br></br>
+               <a href="#!" class="secondary-content"><i class="material-icons">View All Fen Creek Recruits</i></a>
+            </p>
+            <br></br>
+          </li>
+          <li class="collection-item avatar">
+            <span class="title"><strong> Broadway Academy</strong></span>
+            <p>Phone Number: 636-555-4433 <br></br>
+               Coach Name:   Brad Williams <br></br>
+               <a href="#!" class="secondary-content"><i class="material-icons">View All Broadway Academy Recruits</i></a>
+            </p>
+            <br></br>
+          </li>
+          <li class="collection-item avatar">
+            <span class="title"><strong> St. Charles High School</strong></span>
+            <p>Phone Number: 636-777-0008 <br></br>
+               Coach Name:   Chris Jacobs <br></br>
+               <a href="#!" class="secondary-content"><i class="material-icons">View All St. Charles High School Recruits</i></a>
+            </p>
+            <br></br>
+          </li>
+          <li class="collection-item avatar">
+            <span class="title"><strong> East City High School</strong></span>
+            <p>Phone Number: 134-334-7777 <br></br>
+               Coach Name:   John Palmer <br></br>
+               <a href="#!" class="secondary-content"><i class="material-icons">View All East City High School Recruits</i></a>
+            </p>
+            <br></br>
+          </li>
+        </ul>
      </div>
     );
   }
