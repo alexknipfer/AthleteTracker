b5d74bcead2ba31e7806a2f1e0e0e6dd4a02e052
alexknipfer <alexanderknipfer@gmail.com>
2016-02-26 02:50:26 +0000

    made nav visible on login page

--- client/components/Layout/Layout.jsx
+++ client/components/Layout/Layout.jsx
@@ -1,24 +1,12 @@
 Layout = React.createClass({
-    mixins: [ReactMeteorData],
-  
-  getMeteorData() {
-    return {
-      currentUser: Meteor.user()
-    }
-  },
-  
+
    render(){
        return (
            <div>
-           {(() => {
-                if(this.data.currentUser){
-                  return(
-                    <header>
-                        <Navigation />
-                    </header>
-                  );
-                }
-            })()}
+
+            <header>
+                <Navigation />
+            </header>
             
 
             <main className="container">
