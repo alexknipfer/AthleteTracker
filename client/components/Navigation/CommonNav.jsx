e825632522bdc73d91c5ff02d66b4da15bc924f8
alexknipfer <alexanderknipfer@gmail.com>
2016-02-26 02:50:54 +0000

    removed mobile nav from common nav

--- client/components/Navigation/CommonNav.jsx
+++ client/components/Navigation/CommonNav.jsx
@@ -13,12 +13,6 @@ CommonNav = React.createClass({
                 <nav className="indigo z-depth-1">
                   <div className="nav-wrapper">
                     <a href="/" className="brand-logo bold"><i className="material-icons large left">trending_up</i>LU Tracker</a>
-                    <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
-                    
-                    <ul classNameName="right hide-on-med-and-down">
-                    </ul>
-                    <ul className="side-nav" id="mobile-demo">
-                    </ul>
                   </div>
                 </nav>
               </div>
