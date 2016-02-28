22df81f4b90d37bdea0fe5061f42c3d12f83c257
alexknipfer <alexanderknipfer@gmail.com>
2016-02-28 01:40:12 +0000

    changed text

--- client/components/ManageStudents/StudentCard.jsx
+++ client/components/ManageStudents/StudentCard.jsx
@@ -35,7 +35,7 @@ StudentCard = React.createClass({
               <div className="card-content">
               <a href="#"><i className="material-icons red-text" onClick={this.removeStudent.bind(null, student._id, student.firstname, student.lastname)}>clear</i></a>
                 <span className="card-title activator grey-text text-darken-4">{student.firstname} {student.lastname}<i className="material-icons right">reorder</i></span>
-                <p><a href="#">Email Student</a></p>
+                <p><a href="/email">Email Student</a></p>
               </div>
               <div className="card-reveal">
                 <span className="card-title grey-text text-darken-4">Student Name<i className="material-icons right">close</i></span>
