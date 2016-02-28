75ca8b2d007061ab8802a7d1c2c59722d904f605
alexknipfer <alexanderknipfer@gmail.com>
2016-02-28 01:39:41 +0000

    added email page

---
+++ client/components/Email/Email.jsx
@@ -0,0 +1,43 @@
+Email = React.createClass({
+  render(){
+    return (
+     <div>
+        <div className="row">
+          <div col="s12 m4 l4">
+            <a className="btn-floating btn-large waves-effect waves-light red right float-button z-depth-1" href="/AddSchool"><i className="material-icons">add</i></a>
+          </div>
+        </div>
+        <ul className="collection">
+          <li className="collection-item avatar">
+            <span className="title"><strong> 1/1/2016</strong></span>
+            <p>Subject: Email Subject Goes Here <br></br>
+               Text:   Email body goes here <br></br>
+            </p>
+            <br></br>
+          </li>
+          <li className="collection-item avatar">
+            <span className="title"><strong> 1/2/2016</strong></span>
+            <p>Subject: Email Subject Goes Here <br></br>
+               Text:   Email body goes here <br></br>
+            </p>
+            <br></br>
+          </li>
+          <li className="collection-item avatar">
+            <span className="title"><strong> 1/3/2016</strong></span>
+            <p>Subject: Email Subject Goes Here <br></br>
+               Text:   Email body goes here <br></br>
+            </p>
+            <br></br>
+          </li>
+          <li className="collection-item avatar">
+            <span className="title"><strong> 1/4/2016</strong></span>
+            <p>Subject: Email Subject Goes Here <br></br>
+               Text:   Email body goes here <br></br>
+            </p>
+            <br></br>
+          </li>
+        </ul>
+     </div>
+    );
+  }
+});
