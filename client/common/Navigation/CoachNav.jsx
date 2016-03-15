1cf7de5daf73545639041298e9a51e5892e5dd29
alexknipfer <alexanderknipfer@gmail.com>
2016-03-15 20:08:30 +0000

    added navigation for coach

---
+++ client/common/Navigation/CoachNav.jsx
@@ -0,0 +1,50 @@
+CoachNav = React.createClass({
+  mixins: [ReactMeteorData],
+
+  getMeteorData(){
+    const handle = Meteor.subscribe("currentUser");
+
+    return{
+      currentUser: Meteor.users.findOne({
+        _id: Meteor.userId()
+      })
+    };
+  },
+
+  componentDidMount() {
+    $('.button-collapse').sideNav({
+      menuWidth: 210,
+      edge: 'left',
+      closeOnClick: true
+    });
+  },
+
+  logout(){
+    Meteor.logout();
+    Bert.alert( "Goodbye, " + this.data.currentUser.firstname + "!", "success", "fixed-top");
+    FlowRouter.go("/");
+  },
+
+  render(){
+    return(
+      <div className="navbar-fixed">
+        <nav className="indigo">
+          <div className="nav-wrapper">
+            <a href="/manageStudents" className="brand-logo bold"><i className="material-icons large left">trending_up</i>LU Tracker</a>
+            <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+            <ul className="right hide-on-med-and-down">
+              <li><a href="/schools">Schools</a></li>
+              <li><a href="/manageStudents">Manage Students</a></li>
+              <li><a onClick={this.logout}>Logout</a></li>
+            </ul>
+            <ul className="side-nav" id="mobile-demo">
+              <li><a href="/schools">Schools</a></li>
+              <li><a href="/manageStudents">Manage Students</a></li>
+              <li><a onClick={this.logout}>Logout</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+      );
+  }
+});
