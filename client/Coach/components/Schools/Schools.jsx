dfe60da9e01792285cba88c02f62926759500165
alexknipfer <alexanderknipfer@gmail.com>
2016-03-17 18:15:20 +0000

    now adds schools to page

--- client/Coach/components/Schools/Schools.jsx
+++ client/Coach/components/Schools/Schools.jsx
@@ -1,43 +1,37 @@
 Schools = React.createClass({
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    const handle = Meteor.subscribe("schools");
+    return {
+      loading: !handle.ready(),
+      mySchools: SchoolData.find({
+        createdBy: Meteor.userId()
+      }).fetch()
+    };
+  },
+
   render(){
-    return (
-     <div>
-        <div className="row">
-          <div col="s12 m4 l4">
-            <a className="btn-floating btn-large waves-effect waves-light red right float-button z-depth-1" href="/AddSchool"><i className="material-icons">add</i></a>
+   if (this.data.loading) {
+      return (
+        <Loading />
+      );
+    }
+
+    else{
+      return (
+       <div>
+          <div className="row">
+            <div col="s12 m4 l4">
+              <a className="btn-floating btn-large waves-effect waves-light red right float-button z-depth-1" href="/AddSchool"><i className="material-icons">add</i></a>
+            </div>
           </div>
-        </div>
-        <ul className="collection">
-          <li className="collection-item avatar">
-            <span className="title"><strong> Fen Creek High School</strong></span>
-            <p>Phone Number: 314-999-9000 <br></br>
-               Coach Name:   Steven Lewis <br></br>
-            </p>
-            <br></br>
-          </li>
-          <li className="collection-item avatar">
-            <span className="title"><strong> Broadway Academy</strong></span>
-            <p>Phone Number: 636-555-4433 <br></br>
-               Coach Name:   Brad Williams <br></br>
-            </p>
-            <br></br>
-          </li>
-          <li className="collection-item avatar">
-            <span className="title"><strong> St. Charles High School</strong></span>
-            <p>Phone Number: 636-777-0008 <br></br>
-               Coach Name:   Chris Jacobs <br></br>
-            </p>
-            <br></br>
-          </li>
-          <li className="collection-item avatar">
-            <span className="title"><strong> East City High School</strong></span>
-            <p>Phone Number: 134-334-7777 <br></br>
-               Coach Name:   John Palmer <br></br>
-            </p>
-            <br></br>
-          </li>
-        </ul>
-     </div>
-    );
+          <ul className="collection">
+            <SchoolCard schoolData={this.data.mySchools} />
+          </ul>
+       </div>
+      );
   }
+    }
+
 });
