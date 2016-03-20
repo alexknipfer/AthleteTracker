b7f0a5ad2ece40258301df8163de72c20cec8c5a
alexknipfer <alexanderknipfer@gmail.com>
2016-03-20 00:58:17 +0000

    check for user authorization and if no coaches exist

--- client/Admin/components/CurrentCoaches/CurrentCoaches.jsx
+++ client/Admin/components/CurrentCoaches/CurrentCoaches.jsx
@@ -7,28 +7,69 @@ CurrentCoaches = React.createClass({
       loading: !handle.ready(),
       myCoaches: Meteor.users.find({
         roles: "coach"
-      }).fetch()
+      }).fetch(),
+      myCoachCount: Meteor.users.find({
+        roles: "coach"
+      }).count()
     };
   },
 
   render(){
-    return(
-      <div className="row">
-      <div className="col s12 m12 l12">
-        <div className="card">
-          <div className="card-content black-text">
-            <span className="card-title">Current Coaches</span>
-              <div className="row">
-                <div col="s12 m4 l4">
-                  <a className="btn-floating btn-large waves-effect waves-light red right float-button z-depth-1" href="/AddCoach"><i className="material-icons">add</i></a>
+    if (this.data.loading) {
+      return (
+        <Loading />
+      );
+    }
+
+    else if (Meteor.loggingIn()) {
+      return (
+        <Loading />
+      );
+    }
+
+    else if(Roles.userIsInRole(Meteor.userId(), "admin"))
+    {
+      return(
+        <div>
+          <div className="row">
+            <div col="s12 m4 l4">
+            <a className="btn-floating btn-large waves-effect waves-light red right float-button z-depth-1" href="/AddCoach"><i className="material-icons">add</i></a>
+            </div>
+          </div>
+
+            {(() => {
+            if(this.data.myCoachCount == 0){
+              return(
+                <NoCoaches />
+              );
+            }
+
+            else{
+              return(
+              <div>
+                <div className="row">
+                  <div className="col s12 m12 l12">
+                    <div className="card">
+                      <div className="card-content black-text">
+                        <span className="card-title">Current Coaches</span>
+
+                        <CoachTable coachData={this.data.myCoaches} />
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-
-              <CoachTable coachData={this.data.myCoaches} />
-          </div>
+              );
+            }
+          })()}
         </div>
-      </div>
-    </div>
       );
+    }
+
+    else{
+      return(<Unauthorized />);
+    }
+
+
   }
 });
