958999ed971490b41585b2082bd467d895c61664
alexknipfer <alexanderknipfer@gmail.com>
2016-03-24 16:41:31 +0000

    updated published data

--- server/publications/students.js
+++ server/publications/students.js
@@ -4,12 +4,21 @@ Meteor.publish("students", function() {
     }, {
         fields: {
             firstname: 1,
-            middleInitial: 1,
             lastname: 1,
-            school: 1,
+            gradYear: 1,
             gpa: 1,
             act: 1,
-            sat: 1,
+            dob: 1,
+            phoneNumber: 1,
+            email: 1,
+            city: 1,
+            state: 1,
+            height: 1,
+            weight: 1,
+            speedHalf: 1,
+            speedFull: 1,
+            handOrientation: 1,
+            position: 1,
             createdBy: 1
         }
     });
