fb640f17f7c3f5898940ada337a0f4e4054c46eb
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-04-08 10:07:33 -0500

    added a remove button to remove school

--- client/Coach/components/Schools/SchoolCard.js
+++ client/Coach/components/Schools/SchoolCard.js
@@ -1,21 +1,62 @@
 import React from 'react';
 
 export default class SchoolCard extends React.Component{
+
+  removeSchool(id, schoolName, schoolPhone, schoolCoach) {
+    let previousWindowKeyDown = window.onkeydown;
+      swal({
+        title: "Are you sure?",
+        text: "You will not be able to recover " + schoolName + "!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it!",
+        closeOnConfirm: false
+      }, (isConfirm) => {
+        window.onkeydown = previousWindowKeyDown;
+        if(isConfirm){
+          swal('Deleted!', 'User has been deleted.', 'success');
+          Meteor.call("removeSchool", id);
+        }
+        else{
+          swal('Cancelled', 'Your user is safe now.', 'error');
+        }
+      });
+    }
+
   render(){
     return(
       <div>
         {this.props.schoolData.map((schools) =>{
-        return (
-          <li className="collection-item avatar" key={schools._id}>
-            <span className="title"><strong>{schools.schoolName}</strong></span>
-              <p>Phone Number: {schools.schoolPhone}<br></br>
-                  Coach Name:  {schools.schoolCoach}<br></br>
+          return (
+            <li
+              className="collection-item avatar"
+              key={schools._id}>
+              <span className="title">
+                <strong>
+                  {schools.schoolName}
+                </strong>
+                <a href="#">
+                  <i
+                    className="material-icons red-text right"
+                    onClick={this.removeSchool.bind(this, schools._id, schools.schoolName, schools.schoolPhone, schools.schoolCoach)}>close</i>
+                </a>
+              </span>
+
+              <p>
+                Phone Number: {schools.schoolPhone}
+                <br>
+                </br>
+                Coach Name:  {schools.schoolCoach}
+                <br>
+                </br>
               </p>
-              <br></br>
-          </li>
+              <br>
+              </br>
+            </li>
           );
         })}
       </div>
-      );
+    );
   }
 }
