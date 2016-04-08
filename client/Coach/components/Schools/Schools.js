68f7185f478d6efe834edfc1fe8032d5a5ba3ee7
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-04-08 10:07:02 -0500

    added alert to prompt user before removing school

--- client/Coach/components/Schools/Schools.js
+++ client/Coach/components/Schools/Schools.js
@@ -2,34 +2,62 @@ import React from 'react';
 import Loading from '../../../common/components/Loading/Loading.js';
 import Unauthorized from '../../../common/components/Unauthorized/Unauthorized.js';
 import SchoolCard from '../Schools/SchoolCard.js';
+import NoSchools from '../Schools/NoSchools.js';
 
 
 export default class Schools extends React.Component{
 
   render(){
-   if (this.props.loading) {
+    if (this.props.loading) {
       return (
         <Loading />
       );
     }
 
     else if(Roles.userIsInRole(Meteor.userId(), "coach")){
-      return (
-       <div>
+      return(
+        <div>
           <div className="row">
             <div col="s12 m4 l4">
-              <a className="btn-floating btn-large waves-effect waves-light red right float-button z-depth-1" href="/AddSchool"><i className="material-icons">add</i></a>
+              <a
+                className="btn-floating btn-large waves-effect waves-light red right float-button z-depth-1"
+                href="/AddSchool">
+                <i className="material-icons">add</i>
+              </a>
             </div>
           </div>
-          <ul className="collection">
-            <SchoolCard schoolData={this.props.mySchools} />
-          </ul>
-       </div>
+
+          {(() => {
+            if(this.props.mySchoolCount == 0){
+              return(
+                <NoSchools />
+              );
+            }
+
+            else{
+              return (
+                <div>
+                  <ul className="collection">
+                    <SchoolCard schoolData={this.props.mySchools} />
+                  </ul>
+                </div>
+              );
+            }
+          })()}
+        </div>
       );
-    }
+
+
+
+
+
+
+    } //end roles check
 
     else{
-      return(<Unauthorized />);
+      return(
+        <Unauthorized />
+      );
     }
   }
 
