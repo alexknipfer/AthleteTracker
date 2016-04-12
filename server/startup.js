0331d07d1132d43a2a84b66ffbedfe237eb141dd
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-04-12 15:56:01 -0500

    added mail url

--- server/startup.js
+++ server/startup.js
@@ -1,4 +1,7 @@
 Meteor.startup(function() {
+
+  process.env.MAIL_URL = 'smtp://alexknipfer:theunleahsed1238@smtp.sendgrid.net:587';
+
   if (Meteor.users.find().count() === 0) {
     let userAdmin = Accounts.createUser({
       username: "admin",
