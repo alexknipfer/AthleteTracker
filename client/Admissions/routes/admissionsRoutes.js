3ffaf74e5881116ab7236e4cbdd5c90d05fd1c04
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-04-12 16:01:19 -0500

    fixed admissions login bug

--- client/Admissions/routes/admissionsRoutes.js
+++ client/Admissions/routes/admissionsRoutes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'react-mounter';
+import Layout from '../../common/components/Layout/Layout.js';
 import EnrolledStudents from '../components/EnrolledStudents/EnrolledStudents.js';
 
 const admissionsRoutes = FlowRouter.group({
