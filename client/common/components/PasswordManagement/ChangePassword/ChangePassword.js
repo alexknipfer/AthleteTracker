5bf6a3758be6301cf9d884352f227d00f3474094
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-04-22 08:51:27 -0500

    added return link to change password page

--- client/common/components/PasswordManagement/ChangePassword/ChangePassword.js
+++ client/common/components/PasswordManagement/ChangePassword/ChangePassword.js
@@ -11,58 +11,78 @@ export default class ChangePassword extends React.Component{
     Accounts.changePassword(oldPassword, newPassword, (error, reason) => {
       if(error){
         Alert.error(error.reason, {
-            position: 'bottom',
-            effect: 'stackslide',
-            timeout: 3000
+          position: 'bottom',
+          effect: 'stackslide',
+          timeout: 3000
         });
       }
 
       else{
         Alert.success("Successfully Changed Password!", {
-            position: 'bottom',
-            effect: 'stackslide',
-            timeout: 3000
+          position: 'bottom',
+          effect: 'stackslide',
+          timeout: 3000
         });
         FlowRouter.go("/profile");
       }
     });
   }
 
- render(){
-   if (Roles.userIsInRole(Meteor.userId(), "coach") || Roles.userIsInRole(Meteor.userId(), "admin") || Roles.userIsInRole(Meteor.userId(), "admissions")){
-   return (
-     <div className="row">
-      <div className="col s12 m8 l6 offset-l3 offset-m2">
-        <div className="card login-padding">
-          <div className="card-content black-text">
-            <span className="card-title">Change Password</span>
+  render(){
+    if (Roles.userIsInRole(Meteor.userId(), "coach") || Roles.userIsInRole(Meteor.userId(), "admin") || Roles.userIsInRole(Meteor.userId(), "admissions")){
+      return (
+        <div className="row">
+          <div className="col s12 m8 l6 offset-l3 offset-m2">
+            <div className="card change-password">
+              <a href="/profile" className="right">
+                Return to Profile
+              </a>
+              <div className="card-content black-text">
+                <span className="card-title">
+                  Change Password
+                </span>
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                  <div className="row">
+                    <div className="col s12">
+                      <input
+                        type="password"
+                        ref="oldPassword"
+                        className="validate"
+                        placeholder="Old Password"
+                        minLength={2}
+                        required />
+                    </div>
+                  </div>
 
-            <form onSubmit={this.handleSubmit.bind(this)}>
-              <div className="row">
-                <div className="col s12">
-                  <input type="password" ref="oldPassword" className="validate" placeholder="Old Password" minLength={2} required />
-                </div>
+                  <div className="row">
+                    <div className="col s12">
+                      <input
+                        type="password"
+                        ref="newPassword"
+                        className="validate"
+                        placeholder="New Password"
+                        minLength={2}
+                        required />
+                    </div>
+                  </div>
+
+                  <button
+                    className="btn waves-effect login grey"
+                    type="submit">
+                    Change Password
+                  </button>
+                </form>
               </div>
-
-              <div className="row">
-                <div className="col s12">
-                  <input type="password" ref="newPassword" className="validate" placeholder="New Password" minLength={2} required />
-                </div>
-              </div>
-
-              <button className="btn waves-effect login grey" type="submit">Change Password</button>
-            </form>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    );
+      );
+    }
+    else{
+      return(
+        <Unauthorized />
+      );
+    }
   }
-  else{
-    return(
-      <Unauthorized />
-    );
-  }
- }
 
 }
