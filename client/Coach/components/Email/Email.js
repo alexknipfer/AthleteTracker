9908a91b666e2aaf1927faa2cc42626f07b5b886
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-04-22 19:52:19 -0500

    added email form to email page

--- client/Coach/components/Email/Email.js
+++ client/Coach/components/Email/Email.js
@@ -1,56 +1,64 @@
 import React from 'react';
 import Unauthorized from '../../../common/components/Unauthorized/Unauthorized.js';
+import Loading from '../../../common/components/Loading/Loading.js';
 
 export default class Email extends React.Component{
   render(){
-    if(Roles.userIsInRole(Meteor.userId(), "coach")){
+
+    if (Meteor.loggingIn()) {
       return (
-       <div>
-          <div className="row">
-            <div col="s12 m4 l4">
-              <a className="btn-floating btn-large waves-effect waves-light red right float-button z-depth-1" href="/AddSchool"><i className="material-icons">add</i></a>
+        <Loading />
+      );
+    }
+
+    else if(Roles.userIsInRole(Meteor.userId(), "coach")){
+      return (
+        <div className="row">
+          <div className="col s12 m8 l6 offset-l3 offset-m2">
+            <div className="card login-padding">
+              <div className="card-content black-text">
+                <span className="card-title">
+                  Email Athlete
+                </span>
+                <form>
+                  <div className="row">
+                    <div className="input-field col l12">
+                      <label className="active" for="email_address">To:</label>
+                      <input
+                        id="email_address"
+                        type="text"
+                        className="validate" />
+                    </div>
+                  </div>
+
+                  <div className="row">
+                    <div className="input-field col l12">
+                      <textarea
+                        id="email_subject"
+                        className="materialize-textarea">
+                      </textarea>
+                      <label for="email_subject">Subject:</label>
+                    </div>
+                  </div>
+
+                  <button
+                    className="btn waves-effect waves-light"
+                    type="submit"
+                    name="action">
+                    Send Email
+                    <i className="material-icons right">send</i>
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
-          <ul className="collection">
-            <li className="collection-item avatar">
-              <span className="title"><h4>Currently viewing all emails for student name</h4></span>
-            </li>
-            <li className="collection-item avatar">
-              <span className="title"><strong> 1/1/2016</strong></span>
-              <p>Subject: Email Subject Goes Here <br></br>
-                 Text:   Email body goes here <br></br>
-              </p>
-              <br></br>
-            </li>
-            <li className="collection-item avatar">
-              <span className="title"><strong> 1/2/2016</strong></span>
-              <p>Subject: Email Subject Goes Here <br></br>
-                 Text:   Email body goes here <br></br>
-              </p>
-              <br></br>
-            </li>
-            <li className="collection-item avatar">
-              <span className="title"><strong> 1/3/2016</strong></span>
-              <p>Subject: Email Subject Goes Here <br></br>
-                 Text:   Email body goes here <br></br>
-              </p>
-              <br></br>
-            </li>
-            <li className="collection-item avatar">
-              <span className="title"><strong> 1/4/2016</strong></span>
-              <p>Subject: Email Subject Goes Here <br></br>
-                 Text:   Email body goes here <br></br>
-              </p>
-              <br></br>
-            </li>
-          </ul>
-       </div>
-     );
-   }
-   else{
-     return(
-       <Unauthorized />
-     );
-   }
+        </div>
+      );
+    }
+    else{
+      return(
+        <Unauthorized />
+      );
+    }
   }
 }
